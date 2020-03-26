a8d31891af7d5ba35f02a132f47e47fcab279317
shubham52 <shubham@91social.com>
2020-03-26 18:45:01 +0530

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+function slideUp(id){
+    // let div = document.getElementById(id)
+    // let idDiv = id+"Div";
+    // let btnDiv = document.getElementById(idDiv);
+    // console.log(div.style.marginBottom);
+    // if(div.style.marginBottom === "60px"){
+    //     div.style.marginBottom = "0px"; 
+    //     btnDiv.style.marginTop = "-60px";   
+    // }
+    // else{
+    //     div.style.marginBottom = "60px";
+    //     btnDiv.style.marginTop = "0px";
+    // }
+}
